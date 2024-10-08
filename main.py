32c79edd143cb6bdd1fff41c8a40fb175822da11
Reberpower1 <bmsamendes1@gmail.com>
2024-10-08 19:25:07 +0100

    Função Sub

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 def soma(num1, num2):
   return num1 + num2
 
+def sub(num1, num2):
+  return num1 - num2
 
 def main():
-  resul = soma(3+2)
-  print(resul)
+  resul = soma(3, 2)
+  print("3 + 2 = " + str(resul))
+  resul = sub(3, 2)
+  print("3 - 2 = " + str(resul))
   
 if __name__ == "__main__":
   main()
